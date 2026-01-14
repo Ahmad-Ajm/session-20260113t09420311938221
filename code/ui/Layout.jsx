b5070af95a58f7ef4a6e1a313bbe1c6bf84a3831
Ahmad-Ajm <ahaj82@hotmail.com>
2026-01-14 11:51:30 +0300

    add Layout

--- code/ui/Layout.jsx
+++ code/ui/Layout.jsx
@@ -1,13 +1,13 @@
-import Header from './Header';
-import Footer from './Footer';
-import Content from './Content';
+import Header from './Header'
+import Footer from './Footer'
+import Content from './Content'
 
 export default function Layout() {
   return (
-    <div className="app-container" dir="rtl">
+    <div className="layout">
       <Header />
       <Content />
       <Footer />
     </div>
-  );
+  )
 }
